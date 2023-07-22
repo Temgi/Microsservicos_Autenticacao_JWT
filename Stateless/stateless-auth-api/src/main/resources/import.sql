1e92dc2cc948004395e26135febd4983a2e2cee2
Rui Temgi <ruisaraivasp@gmail.com>
2023-07-22 13:13:50 -0300

    Connect Applications in Postgresql

---
+++ Stateless/stateless-auth-api/src/main/resources/import.sql
@@ -0,0 +1,6 @@
+-- 123456 password
+INSERT INTO public.user (id, username, password) VALUES (1, 'user.test', '$2a$10$NTGaffJyIx3Z0Q4rEV0UluRht5DaxBGVIzdO1/m0reurI7b3K4/ti');
+
+
+-- 456789 password
+INSERT INTO public.user (id, username, password) VALUES (2, 'user.new.test', '$2a$10$NTGaffJyIx3Z0Q4rEV0UluRht5DaxBGVIzdO1/m0reurI7b3K4/ti');
